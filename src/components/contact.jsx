b07890e5649bcddbb334e6c7f10ddf22cda870e3
Viachaslau Bohdan <slawandr1@gmail.com>
2025-05-01 00:20:40 +0100

    Update website content and structure: - Add floating WhatsApp and Telegram buttons - Comment out social media section in footer - Update contact information - Update service icons and descriptions - Clean up navigation menu - Update meta information

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -125,29 +125,6 @@ export const Contact = (props) => {
               </p>
             </div>
           </div>
-          <div className="col-md-12">
-            <div className="row">
-              <div className="social">
-                <ul>
-                  <li>
-                    <a href={props.data ? props.data.facebook : "/"}>
-                      <i className="fa fa-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
       <div id="footer">
@@ -155,7 +132,7 @@ export const Contact = (props) => {
           <p>
             &copy; 2024. All Rights Reserved.
             <a href="http://www.tiger-web-solutions.com" rel="nofollow">
-              PromptAI Tech - IT Services.
+              PromptAI Studio
             </a>
           </p>
         </div>
